8386f316e25fc562ee97aeb2b7c2f6a3ab972484
恵太 川満 <keitakawamitsu0980@gmail.com>
2023-02-07 21:58:54 +0900

    [update]構文解析のメソッド追加。

--- kmt_text.py
+++ kmt_text.py
@@ -1,4 +1,6 @@
 # coding:utf-8
+import re
+import unicodedata
 from maya import cmds as cmds
 
 class Text:
@@ -33,3 +35,43 @@ class Text:
         cmds.parent(shape_list,r=1,s=1)
         cmds.delete(trans)
         cmds.rename(_shape,shape_name)
+
+
+        def hoge(self):
+            """構文解析用
+            """
+            text = "10F~44f"
+            small_text = unicodedata.normalize('NFKC',text)
+            new_list = []
+            for i in small_text:
+                """mayaが使えない文字はアンスコに変換する
+                """
+                if re.search("[0-9]+",i):
+                    new_list.append(i)
+                elif re.search("[a-zA-Z]+",i):
+                    new_list.append(i)
+        
+                elif re.search("\W",i):
+                    if i == "~":
+                       _ = i.replace(i,"~")
+                       new_list.append(_)
+                    else:
+                        _ = i.replace(i,"_")
+                        new_list.append(_)
+        
+                elif re.compile('[\u3041-\u309F]+'):
+                    print(i)
+                    _ = i.replace(i,"_")
+                    new_list.append(_)    
+                else:
+                    new_list.append(i)
+        
+            _re_text = "".join(new_list)
+            print(_re_text)
+    
+            curves_text = cmds.textCurves( f='times-roman',t= _re_text)[0]
+            result1 = re.split("_",curves_text)[1]
+            new_curves_text = curves_text.replace(result1,_re_text)
+            cmds.rename(curves_text,new_curves_text)
+
+
