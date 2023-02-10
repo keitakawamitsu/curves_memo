62dd4b3763813500bc58f5d932ae8834cfe703bd
恵太 川満 <keitakawamitsu0980@gmail.com>
2023-02-10 22:13:02 +0900

    [update]リファクタ中、、、。 カーブ作る系と構文解析系はクラスを分けた。 重複回避やシェイプのリネーム以前に、まずはカーブを作った後のtransformノードのリネームから実装、 Scene内の重複調査はその後かと思います...

--- kmt_text.py
+++ kmt_text.py
@@ -3,10 +3,10 @@ import re
 import unicodedata
 from maya import cmds as cmds
 
-class Text:
+class MakeCurves:
     def __init__(self,text):
         self.text = text
-        pass
+        self.tx = Text(self.text)
 
     def get_text(self):
         small_text = unicodedata.normalize('NFKC',self.text)
@@ -40,8 +40,8 @@ class Text:
         
         return new_curves_text,textCurvesNode
     
-    def make_shape(self):
-        cv_name,textCurvesNode = self.get_text()
+    def make_shape(self,cv_name,textCurvesNode):
+        #cv_name,textCurvesNode = self.get_text()
         print("----------- cv_name is --------")
         print(cv_name)
 
@@ -57,25 +57,23 @@ class Text:
         shape_list.append(cv_name)
         cmds.parent(shape_list,r=1,s=1)
         cmds.delete(cmds.listRelatives(cv_name,typ="transform"))
+        print(shape_list)
 
-    def is_node(self):
-        pas_name = self.parsing(self.text)
+    def rename_transform(self,*arg):
+        pas_text = self.tx.parsing()
+        pattern = r'^[Text]{1,4}_(.*?)_\d{1,3}$'
         allNodes = cmds.ls(typ="transform")
-        pattern = r'^[a-zA-Z]{1,4}_(.*?)_\d{1,3}$'#先頭が1~4文字以内のアルファベット、最後が1~3桁の数字、真ん中はなんでもOK
-    
-        for self.node in allNodes:
-            a=re.search(pattern,self.node)
+        new_text =""
+        for i in allNodes:#カーブ作った後のtransformノード調べて、matchする部分を取得
+            a=re.search(pattern,i)
             if a:
-                match_name = a.group(1)
-                if match_name == pas_name:
-
-                    print(f"{pas_name}はSceneにあるので違う名前にして{pas_name}を作る")
-                    num = 1
-                    return num
+                str = a.group(1)
+                new_text = i.replace(str,pas_text)#パースされた文字と置き換え
+                cmds.rename(i,new_text)
+                break
+            
             else:
-                print(f"{pas_name}はないのでnode作る")
-                num = 0
-                return num
+                print(f"{i} is non match node")
 
     def rename_node(self,result):
         """Scene内のノードを探して、重複してたら新しい名前を返す
@@ -95,12 +93,33 @@ class Text:
             return 0
             #self.get_text(self.text)
 
-    def parsing(self,in_text):
+    def main(self):
+        #カーブ作る
+        #if Mayaで使える文字列ですか？:
+        #   使える文字列に変換する
+        #if それはシーンの中にありますか？
+        #   あったら文字列変更
+        #   なかったらスルー
+        #シェイプマージ
+        #
+        pass
+    def make_curves(self,input_text):
+       cv_name,textCurvesNode = cmds.textCurves( f='times-roman',t = input_text)#カーブの生成はこれくらいシンプルにしないとアカン
+       print(cv_name)
+       self.rename_transform()
+       return cv_name,textCurvesNode
+
+
+class Text:
+    def __init__(self, text=""):
+        self.text = text
+
+    def parsing(self):
         """構文解析用。mayaが使えない文字はアンスコに変換する
         """
         print("-------in_text is --------")
-        print(in_text)
-        small_text = unicodedata.normalize('NFKC',in_text)
+        print(self.text)
+        small_text = unicodedata.normalize('NFKC',self.text)
         new_list = []
 
         print("-------small_text is --------")
@@ -127,3 +146,43 @@ class Text:
         print("------------------parsing self.response is ------------------")
         print(self.response)
         return self.response
+    
+    def is_node(self,parsing_text):
+        """パースされた文字を受け取り、その名前でScene内を探し、重複があったら変更用のフラグを返す
+        """
+        pas_name = parsing_text
+        allNodes = cmds.ls(typ="transform")
+        pattern = r'^[a-zA-Z]{1,4}_(.*?)_\d{1,3}$'#先頭が1~4文字以内のアルファベット、最後が1~3桁の数字、真ん中はなんでもOK
+    
+        for self.node in allNodes:
+            a=re.search(pattern,self.node)
+            if a:
+                match_name = a.group(1)
+                if match_name == pas_name:
+
+                    print(f"{pas_name}はSceneにあるので違う名前にして{pas_name}を作る")
+                    num = 1
+                    return num
+            else:
+                print(f"{pas_name}はないのでnode作ってOK")
+                num = 0
+                return num
+    
+    
+    def rename_node(self,result):
+        """Scene内のノードを探し、重複してたら新しい名前にして返す
+        """
+        #result = self.is_node()
+        if result:
+            b = re.search('[0-9]{1,3}$',self.node)
+            num = int(b.group(0))
+            num += 1
+
+            c = re.search(r'(.*?)\d{1,3}$',self.node)
+            self.new_name = c.group(1)+str(num)
+            return self.new_name
+
+        else:
+            print("普通にnode作る")
+            return 0
+            #self.get_text(self.text)
