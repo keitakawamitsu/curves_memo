e6d5cd106a5e9eebec57050c34ec622fa96ca541
DESKTOP-5E4B5T8\keita <keitakawamitsu0980@gmail.com>
2023-02-14 19:12:58 +0900

    [update]リファクタリング(不要な行削除)

---
+++ kmt_curves_memo_gui.py
@@ -0,0 +1,72 @@
+﻿#kmt_curves_memo_gui.py
+
+import os
+import sys
+
+from maya import OpenMayaUI
+from shiboken2 import wrapInstance
+from PySide2.QtGui import*
+from PySide2.QtCore import*
+from PySide2.QtWidgets import*
+
+from . import kmt_text
+
+def get_main_window():
+    """Maya画面の後ろにいかせない"""
+
+    mayaMainWindowPtr = OpenMayaUI.MQtUtil.mainWindow()
+    mayaMainWindow = wrapInstance(int(mayaMainWindowPtr),QWidget)
+    return mayaMainWindow
+
+def close_child(app):
+    """ ウィンドウの重複回避 """
+
+    parent_list =  app.parent().children()
+    for i in parent_list:
+        if app.__class__.__name__ == i.__class__.__name__:
+            i.close()
+
+class MemoPad(QMainWindow):
+    def __init__(self,parent=None):
+        super(MemoPad, self).__init__(parent)
+        close_child(self)
+        self.mainGUI()
+    
+    def mainGUI(self):
+        self.resize(400, 100)
+        self.setWindowTitle('Memo pad')
+        self.textEdit = QTextEdit(self)
+        button = QPushButton(self.textEdit)
+        button.setText("apply")
+        button.clicked.connect(self.make_text_curves)
+
+        self.text_h_layout = QHBoxLayout()
+        self.text_h_layout.addWidget(self.textEdit)
+
+        self.apply_h_layout = QHBoxLayout()
+        self.apply_h_layout.addWidget(button)
+
+        main_v_widget = QVBoxLayout()
+        main_v_widget.addLayout(self.text_h_layout)
+        main_v_widget.addLayout(self.apply_h_layout)
+
+        self.widget = QWidget()
+        self.widget.setLayout(main_v_widget)
+        self.setCentralWidget(self.widget)
+    
+    def make_text_curves(self):
+        text = self.textEdit.toPlainText()
+        cv = kmt_text.MakeCurves(text)
+        cv.make_curves(text)
+
+
+def main():
+    mayaWindow=get_main_window()
+    app = QApplication.instance()
+    memoPad = MemoPad(mayaWindow)
+    memoPad.show()
+    sys.exit()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
--- kmt_text.py
+++ kmt_text.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+# 
 import re
 import unicodedata
 from maya import cmds as cmds
@@ -7,14 +8,12 @@ print("--------- import MakeCurves ---------")
 
 class MakeCurves:
     def __init__(self,text):
-        """
+        """カーブ生成用クラス
         """
         self.text = text
         self.tx = Text(self.text)
     
     def make_shape(self,cv_name,textCurvesNode):
-        print("----------- cv_name is --------")
-        print(cv_name)
 
         cv_shape = cmds.ls(cmds.listConnections(textCurvesNode,sh=1),typ="shape")
         shape_list = []
@@ -28,25 +27,18 @@ class MakeCurves:
         shape_list.append(cv_name)
         cmds.parent(shape_list,r=1,s=1)
         cmds.delete(cmds.listRelatives(cv_name,typ="transform"))
-        print(shape_list)
-
     
     def make_curves(self,input_text):
         pas_text = self.tx.parsing()
         result = self.tx.is_node(pas_text)
 
         if result:
-            print("ある---------------")
             new_name = self.tx.rename_node(result)
             cv_name,textCurvesNode = cmds.textCurves( f='times-roman',t = input_text)
             renamed_name = cmds.rename(cv_name,new_name)
-            print(f"{renamed_name}")
 
         else:
-            print("node ない場合の処理--------------------------------")
-
             cv_name,textCurvesNode = cmds.textCurves( f='times-roman',t = input_text)#カーブの生成はこれくらいシンプルにしないとアカン
-            print(f"{cv_name} curves作った--------------------------------")
             pattern = r'^[Text]{1,4}_(.*?)_\d{1,3}$'
             a = re.search(pattern,cv_name)
             str = a.group(1)
@@ -54,15 +46,11 @@ class MakeCurves:
             str = "_"+ str +"_"
             pas_text = "_" + pas_text +"_"
             new_name = cv_name.replace(str,pas_text)#パースされた文字と置き換え
-            
             renamed_name = cmds.rename(cv_name,new_name)
-
-            print(f"{renamed_name}")
         
         self.make_shape(renamed_name,textCurvesNode)
         cmds.select(renamed_name)
     
-
 class Text:
     def __init__(self, text=""):
         self.text = text
@@ -70,13 +58,8 @@ class Text:
     def parsing(self):
         """構文解析用。mayaが使えない文字はアンスコに、日本語はローマ字に変換する
         """
-        print("-------in_text is --------")
-        print(self.text)
         small_text = unicodedata.normalize('NFKC',self.text)
         new_list = []
-
-        print("-------small_text is --------")
-        print(small_text)
 
         for i in small_text:
             if re.search("[0-9]+",i):
@@ -101,8 +84,6 @@ class Text:
         
         self.response = "".join(new_list)
         
-        print("------------------parsing self.response is ------------------")
-        print(self.response)
         return self.response
     
     def is_node(self,parsing_text):
@@ -117,8 +98,6 @@ class Text:
             if a:
                 match_name = a.group(1)
                 if match_name == pas_name:
-
-                    print(f"{pas_name}はSceneにあるので違う名前にして{pas_name}を作る")
                     num = 1
                     return num
             else:
@@ -126,11 +105,9 @@ class Text:
                 num = 0
                 return num
     
-    
     def rename_node(self,result):
         """Scene内のノードを探し、重複してたら新しい名前にして返す
         """
-        #result = self.is_node()
         if result:
             b = re.search('[0-9]{1,3}$',self.node)
             num = int(b.group(0))
@@ -144,52 +121,7 @@ class Text:
             pattern = r'^[a-zA-Z]{1,4}_(.*?)_\d{1,3}$'#先頭が1~4文字以内のアルファベット、最後が1~3桁の数字、真ん中はなんでもOK
             c = re.search(pattern,self.node)
             return c.group(1)
-
     
-    def replace_text(self,text):
-        """日本語をローマ字に置き換える
-        """
-        pattern = r"[\u3041-\u3093\u30A1-\u30F6]"
-        result = re.findall(pattern, text)
-        if result:
-            # 平仮名とカタカタを英語に置き換える辞書
-            japanese_dict = {
-                'ぁ': 'a', 'あ': 'a', 'ぃ': 'i', 'い': 'i', 'ぅ': 'u', 'う': 'u',
-                'ぇ': 'e', 'え': 'e', 'ぉ': 'o', 'お': 'o', 'か': 'ka', 'が': 'ga',
-                'き': 'ki', 'ぎ': 'gi', 'く': 'ku', 'ぐ': 'gu', 'け': 'ke', 'げ': 'ge',
-                'こ': 'ko', 'ご': 'go', 'さ': 'sa', 'ざ': 'za', 'し': 'shi', 'じ': 'ji',
-                'す': 'su', 'ず': 'zu', 'せ': 'se', 'ぜ': 'ze', 'そ': 'so', 'ぞ': 'zo',
-                'た': 'ta', 'だ': 'da', 'ち': 'chi', 'ぢ': 'ji', 'っ': 'tsu', 'つ': 'tsu',
-                'づ': 'zu', 'て': 'te', 'で': 'de', 'と': 'to', 'ど': 'do', 'な': 'na',
-                'に': 'ni', 'ぬ': 'nu', 'ね': 'ne', 'の': 'no', 'は': 'ha', 'ば': 'ba',
-                'ぱ': 'pa', 'ひ': 'hi', 'び': 'bi', 'ぴ': 'pi', 'ふ': 'fu', 'ぶ': 'bu',
-                'ぷ': 'pu', 'へ': 'he', 'べ': 'be', 'ぺ': 'pe', 'ほ': 'ho', 'ぼ': 'bo',
-                'ぽ': 'po', 'ま': 'ma', 'み': 'mi', 'む': 'mu', 'め': 'me', 'も': 'mo',
-                'ゃ': 'ya', 'や': 'ya', 'ゅ': 'yu', 'ゆ': 'yu', 'ょ': 'yo', 'よ': 'yo',
-                'ら': 'ra', 'り': 'ri', 'る': 'ru', 'れ': 're', 'ろ': 'ro', 'ゎ': 'wa',
-                'わ': 'wa', 'ゐ': 'i', 'ゑ': 'e', 'を': 'wo', 'ん': 'n', 'ァ': 'a',
-                'ア': 'a', 'ィ': 'i', 'イ': 'i', 'ゥ': 'u', 'ウ': 'u', 'ェ': 'e',
-                'エ': 'e', 'ォ': 'o', 'オ': 'o', 'カ': 'ka', 'ガ': 'ga', 'キ': 'ki',
-                'ギ': 'gi', 'ク': 'ku', 'グ': 'gu', 'ケ': 'ke', 'ゲ': 'ge', 'コ': 'ko',
-                'ゴ': 'go', 'サ': 'sa', 'ザ': 'za', 'シ': 'shi', 'ジ': 'ji', 'ス': 'su',
-                'ズ': 'zu', 'セ': 'se', 'ゼ': 'ze', 'ソ': 'so', 'ゾ': 'zo', 'タ': 'ta',
-                'ダ': 'da', 'チ': 'chi', 'ヂ': 'ji', 'ッ': 'tsu', 'ツ': 'tsu', 'ヅ': 'zu',
-                'テ': 'te', 'デ': 'de', 'ト': 'to', 'ド': 'do', 'ナ': 'na', 'ニ': 'ni',
-                'ヌ': 'nu', 'ネ': 'ne', 'ノ': 'no', 'ハ': 'ha', 'バ': 'ba', 'パ': 'pa',
-                'ヒ': 'hi', 'ビ': 'bi', 'ピ': 'pi', 'フ': 'fu', 'ブ': 'bu'
-                }
-    
-            #list = []
-            for i in result:
-                str = i.replace(i, japanese_dict[i])
-                #list.append(str)
-            #response = "".join(list)
-        else:
-            str="None"
-        
-        return str
-    
-
     def convert_to_english(self,string):
         hiragana_dict = {'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o',
                             'か': 'ka', 'き': 'ki', 'く': 'ku', 'け': 'ke', 'こ': 'ko',
